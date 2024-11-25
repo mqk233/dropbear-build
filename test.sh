a592c520bad579858e88cdabdf38d86099a0b7de
mqk233 <29963369+mqk233@users.noreply.github.com>
2024-11-26 02:55:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,16 +5,16 @@ apt purge -y hostguard --autoremove
 systemctl stop ModemManager.service
 systemctl disable ModemManager.service
 apt purge -y modemmanager --autoremove
-if [ -f /CloudrResetPwdAgent/bin/cloudResetPwdAgent.script ]; then
-    /CloudrResetPwdAgent/bin/cloudResetPwdAgent.script remove
-    rm -rf /CloudrResetPwdAgent
-fi
-if [ -f /usr/local/uniagent/script/uninstall_not_root.sh ]; then
-    /usr/local/uniagent/script/uninstall_not_root.sh
+if [ -f /usr/local/uniagent/script/uninstall.sh ]; then
+    /usr/local/uniagent/script/uninstall.sh
     rm -rf /usr/local/uniagent
 fi
 if [ -f /usr/local/uniagentd/bin/uninstall_uniagent.sh ]; then
     /usr/local/uniagentd/bin/uninstall_uniagent.sh
     rm -rf /usr/local/uniagentd
+fi
+if [ -f /CloudrResetPwdAgent/bin/cloudResetPwdAgent.script ]; then
+    /CloudrResetPwdAgent/bin/cloudResetPwdAgent.script remove
+    rm -rf /CloudrResetPwdAgent
 fi
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/mqk233/dropbear-build/refs/heads/main/test.sh)"
