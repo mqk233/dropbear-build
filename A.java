bac15b6b56517fc98686c5aab1975e94f45ec3e2
mqk233 <29963369+mqk233@users.noreply.github.com>
2026-01-27 01:28:10 +0800

    Update A.java

--- A.java
+++ A.java
@@ -1,5 +1,5 @@
 public class A  {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         System.getProperties().forEach((key, value) -> System.out.println(key + ": " + value));
-}
+    }
 }
